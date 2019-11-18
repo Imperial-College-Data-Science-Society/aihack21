7c88c4978ed782efc69fa72c1b8bb0866314ee91
Tilman Roeder <dyed.green.info@gmail.com>
2019-11-18 19:54:21 +0000

    CSS is very therapeutic

--- anims.js
+++ anims.js
@@ -6,6 +6,8 @@
 // for now this should be fine.
 
 const nav = document.getElementById("nav-header");
+const eventDate = new Date("Mon Nov 28 2019 18:21:12 GMT+0000 (GMT)"); // FIXME
+const underlineTitles = [...document.querySelectorAll(".section h1")]
 
 const state = {
   headerTransparent: nav.classList.contains("nav-transparent"),
@@ -23,6 +25,32 @@ function scrollEvent() {
     if (!state.headerTransparent) nav.classList.add("nav-transparent");
     state.headerTransparent = true;
   }
+
+  // Underline titles
+  underlineTitles.forEach(hdr => {
+    console.log(hdr.offsetTop, scroll);
+    console.log(hdr.offsetTop - scroll);
+    if (hdr.offsetTop - scroll < window.visualViewport.height * 0.6) {
+      hdr.classList.add("underlined");
+    } else {
+      hdr.classList.remove("underlined");
+    }
+  });
 }
 window.addEventListener("scroll", scrollEvent);
 scrollEvent();
+
+// Countdowns
+[...document.getElementsByClassName("countdown")].forEach((elem) => {
+  const days = elem.children[0];
+  const hours = elem.children[1];
+  const minutes = elem.children[2];
+  const update = () => {
+    const diff = eventDate.valueOf() - Date.now(); // ms
+    days.innerHTML = `${Math.floor(diff / 1000 / 60 / 60 / 24)}`;
+    hours.innerHTML = `${Math.floor(diff / 1000 / 60 / 60) % 24}`;
+    minutes.innerHTML = `${Math.floor(diff / 1000 / 60) % 60}`;
+    setTimeout(update, 1000 * 60);
+  };
+  update();
+});
